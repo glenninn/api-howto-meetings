4bef4ae77c3a38a83d62a1f9196c9e5195dcc15c
Glenn Inn <glenninn@yahoo.com>
2017-09-25 15:39:10 -0700

    update with Corporate styling
    add CSS to make into BJN look and feel

--- bjnscripts.js
+++ bjnscripts.js
@@ -10,3 +10,28 @@ function hideit(x){
 	 pi.setAttribute("class", "glyphicon glyphicon-chevron-right");
   }
 }
+
+
+$(document).ready(function () {
+	var menu_is_expanded = false;
+    var mobile = $(window).width()<768? true:false;
+    console.log(mobile);
+	$("#mobile_menu_icon").click(function(){
+		if (!menu_is_expanded) {
+			$("#mobile_menu_icon img").attr("src", "img/close.svg");
+			menu_is_expanded = true;
+		} else {
+			$("#mobile_menu_icon img").attr("src", "img/menu_icon.svg");
+			menu_is_expanded = false;
+		}
+		$(".mobile-menu-list").toggleClass("hidden");
+		$(".mobile-menu-list").toggleClass("mobfixed");
+		$(".mobile-menu-list").css("top", $("header").outerHeight());
+		//return false;
+  	});  	
+});
+
+
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("curr_year").innerHTML = n+'';
